62dc2b25b840fc031491bda528ede5be5d5bf593
Clarence Chiu <clarchiu@gmail.com>
2020-10-29 22:20:48 +0000

    implemented map.js

---
+++ map.js
@@ -0,0 +1,35 @@
+const eqArrays = function(arrA, arrB) {
+  if (arrA.length !== arrB.length) return false;
+
+  for (let i = 0; i < arrA.length; i++) {
+    if (arrA[i] !== arrB[i]) return false;
+  }
+
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
+}
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+}
+
+const results1 = map(words, word => word[0]);
+const results2 = map([1], num => num * 10);
+const results3 = map([], (x) => {x});
+assertArraysEqual(results1, ['g', 'c', 't', 'm', 't']);
+assertArraysEqual(results2, [10]);
+assertArraysEqual(results3, []);
+
